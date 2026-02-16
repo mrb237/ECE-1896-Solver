b58ce048ea5ffd62a08f19e8919812bb9c2e4a86
Michael Bliesath <mrb237@pitt.edu>
2026-02-16 13:51:46 -0500

    Milestone 2

--- Bus.py
+++ Bus.py
@@ -1,9 +1,9 @@
 class Bus:
     index_counter = 1
 
-    def __init__(self, name:str, nominal_kn:float):
+    def __init__(self, name:str, nominal_kv:float):
         self.name = name
-        self.nominal_kn = nominal_kn
+        self.nominal_kv = nominal_kv
         self.bus_index = Bus.index_counter
         Bus.index_counter += 1
 
@@ -11,11 +11,11 @@ if __name__ == "__main__":
     bus1 = Bus("Bus1", 20.0)
 
     print(f"Bus1 name: {bus1.name}")
-    print(f"Bus1 Nominal Voltage: {bus1.nominal_kn} Volts")
+    print(f"Bus1 Nominal Voltage: {bus1.nominal_kv} Volts")
     print(f"Bus1 Index: {bus1.bus_index}")
 
     bus2 = Bus("Bus2", 230.0)
 
     print(f"Bus2 name: {bus2.name}")
-    print(f"Bus2 Nominal Voltage: {bus2.nominal_kn} Volts")
+    print(f"Bus2 Nominal Voltage: {bus2.nominal_kv} Volts")
     print(f"Bus2 Index: {bus2.bus_index}")
